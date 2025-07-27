94509fe454dc28bcdfe782350d89d8442aa3524c
Carlos A. <ch94ca@gmail.com>
2025-07-27 11:58:36 -0300

    Starting project with spring initializr

---
+++ SimpleExpenseManager/src/main/java/com/infnet/simpleExpenseManager/SimpleExpenseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.simpleExpenseManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleExpenseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleExpenseManagerApplication.class, args);
+	}
+
+}
---
+++ SimpleExpenseManager/src/test/java/com/infnet/simpleExpenseManager/SimpleExpenseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.simpleExpenseManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleExpenseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
